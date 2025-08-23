08e478147e74fd1bb76012e57f9dd2d633f5e6d7
nane <gnane5940@gmail.com>
2025-08-24 00:20:56 +0530

    2025-AUG-21

--- docker.tf
+++ docker.tf
@@ -16,29 +16,30 @@ resource "aws_instance" "docker" {
 }
 
 resource "aws_security_group" "allow_all_docker" {
-    name        = "allow_all_docker"
-    description = "allow all traffic"
+  name        = "allow_all_docker"
+  description = "allow all traffic"
 
-    ingress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
-    egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-    lifecycle {
-      create_before_destroy = true
-    }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-    tags = {
-        Name = "allow-all-docker"
-    }
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "allow-all-docker"
+  }
 }
